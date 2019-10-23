81c957f0871d49ab6cadcb057be202d018dd20d1
yangzhou <angenal@hotmail.com>
2019-10-23 21:44:08 +0800

    update

--- sh/vitess.sh
+++ sh/vitess.sh
@@ -12,8 +12,9 @@ export VTTOP=/4g/database/vt
 export VTROOT=/4g/database/vt
 export VTDATAROOT=/4g/database/vt/data
 export MYSQL_FLAVOR=MySQL56
-export PATH=$VTROOT/bin:$GOROOT/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:$JAVA_HOME/bin:$JRE_HOME/bin
+export PATH=$VTROOT/bin:$PATH
+# export PATH=$VTROOT/bin:$GOROOT/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # https://vitess.io/docs/get-started/local
-cd examples/local
+cd $VTROOT/examples/local
 ./101_initial_cluster.sh

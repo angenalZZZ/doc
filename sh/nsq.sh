b401a78334b6cc279a739b99311c88f67fa4a0a9
yangzhou <angenal@hotmail.com>
2019-12-01 16:24:38 +0800

    update

--- sh/nsq.sh
+++ sh/nsq.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh -c '/4g/go/nsq/nsqlookupd &'
-sh -c '/4g/go/nsq/nsqd --lookupd-tcp-address=127.0.0.1:4160 -data-path=/4g/go/nsq/data &'
-sh -c '/4g/go/nsq/nsqadmin --lookupd-http-address=127.0.0.1:4161 -log-level=warn &'
+sh -c '/4g/go/nsq/nsqlookupd -tcp-address=0.0.0.0:4160 -http-address=0.0.0.0:4161 &'
+sh -c '/4g/go/nsq/nsqd -node-id=1 --lookupd-tcp-address=127.0.0.1:4160 -tcp-address=0.0.0.0:4150 -http-address=0.0.0.0:4151 -data-path=/4g/go/nsq/data &'
+sh -c '/4g/go/nsq/nsqadmin --lookupd-http-address=127.0.0.1:4161 -http-address=0.0.0.0:4171 -log-level=warn &'

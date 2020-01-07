85384f579cf72b967bc6fcfb8c03211c13b9f8c0
Angenal <angenal@hotmail.com>
2020-01-07 13:32:31 +0800

    Update and rename .profile to 01-locale-profile.sh

--- 01-locale-profile.sh
+++ 01-locale-profile.sh
@@ -1,3 +1,5 @@
+##文件的路径: /etc/profile.d/01-locale-profile.sh  =>  /etc/profile
+##进行软链结: ln -s /*/doc/sh/01-locale-profile.sh /etc/profile.d/01-locale-profile.sh
 
 ##系统环境设置
 
@@ -7,13 +9,15 @@ export TZ='Asia/Shanghai'
 ##开发环境设置
 
 # go
-export GOROOT=/usr/local/go
-export GOPATH=/20g/go
+export GO111MODULE=auto
 export GOPROXY=https://goproxy.io
+export GOPATH=/20g/go
+export GOROOT=/usr/local/go
+export GOTOOLS=$GOROOT/pkg/tool
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 # java
-export JAVA_HOME=/usr/local/java/jdk1.8.0_212
+export JAVA_HOME=/usr/local/java/jdk1.8.0_221
 export JAVA_BIN=$JAVA_HOME/bin
 export JRE_HOME=$JAVA_HOME/jre
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JRE_HOME/lib
@@ -25,7 +29,7 @@ export PATH=/usr/local/node/bin:$PATH
 ##应用服务设置
 
 # docker
-# export DOCKER_HOST=tcp://127.0.0.1:2375
+export DOCKER_HOST=tcp://127.0.0.1:2375
 
 # other
 

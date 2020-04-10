6114b8ee2790defd9ebe8377cb3b3bf508a2a336
Angenal <angenal@hotmail.com>
2020-04-10 14:06:40 +0800

    Update openssl-gen.sh

--- cert/openssl/openssl-gen.sh
+++ cert/openssl/openssl-gen.sh
@@ -6,3 +6,11 @@ openssl rsa -passin pass:1 -in ca.key.pem -out ca.key.pem.tmp
 mv ca.key.pem.tmp ca.key.pem
 
 openssl req -config openssl.cnf -key ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out ca.pem
+
+# https://www.cnblogs.com/nidey/p/9041960.html
+# 生成秘钥，得到(无密码的) server.key
+# openssl genrsa -out server.key 2048
+# 生成证书签发请求，得到 server.csr
+# openssl req -new -sha256 -out server.csr -key server.key -config openssl.conf
+# 用CA证书生成终端用户证书，得到 server.crt
+# openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -CAcreateserial -in server.csr -out server.crt -extensions req_ext -extfile openssl.conf

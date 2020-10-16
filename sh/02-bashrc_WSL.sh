ad0283ebe9b350ca8a2b27e1665f06d240a8bf92
Angenal <angenal@hotmail.com>
2020-10-16 09:26:21 +0800

    Update 02-bashrc_WSL.sh

--- sh/02-bashrc_WSL.sh
+++ sh/02-bashrc_WSL.sh
@@ -1,13 +1,14 @@
 ##文件的路径:  ~/.bashrc  ~/.zshrc
 ##Windows10/Linux(WSL) 添加如下内容,到文件尾部.
 
-##系统环境设置
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/yangzhou/.antigen/bundles/robbyrussell/oh-my-zsh/lib:/home/yangzhou/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/pip:/home/yangzhou/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/svn-fast-info:/home/yangzhou/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/colorize:/home/yangzhou/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/github:/home/yangzhou/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/python:/home/yangzhou/.antigen/bundles/zsh-users/zsh-autosuggestions:/home/yangzhou/.antigen/bundles/zsh-users/zsh-completions:/home/yangzhou/.antigen/bundles/Vifon/deer:/home/yangzhou/.antigen/bundles/willghatch/zsh-cdr:/home/yangzhou/.antigen/bundles/zsh-users/zaw:/home/yangzhou/.antigen/bundles/zsh-users/zaw/functions:/home/yangzhou/.antigen/bundles/zsh-users/zsh-syntax-highlighting
+##系统环境变量
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export PATH=$PATH:~/.antigen/bundles/robbyrussell/oh-my-zsh/lib:~/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/pip:~/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/svn-fast-info:~/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/colorize:~/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/github:~/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/python:~/.antigen/bundles/zsh-users/zsh-autosuggestions:~/.antigen/bundles/zsh-users/zsh-completions:~/.antigen/bundles/Vifon/deer:~/.antigen/bundles/willghatch/zsh-cdr:~/.antigen/bundles/zsh-users/zaw:~/.antigen/bundles/zsh-users/zaw/functions:~/.antigen/bundles/zsh-users/zsh-syntax-highlighting
 
-# local time
+##本地时区设置
 export TZ='Asia/Shanghai'
 
-##开发环境设置
+##本地开发环境
 
 # go
 export GO111MODULE=auto
@@ -30,9 +31,9 @@ export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 # docker
 export DOCKER_HOST=tcp://127.0.0.1:2375
-# eval $(docker-machine env default)
+# docker_k8s: eval $(docker-machine env default)
 # export DOCKER_TLS_VERIFY="1"
-# export DOCKER_HOST="tcp://192.168.99.100:2376"
+# export DOCKER_HOST="tcp://192.168.1.6:2376"
 # export DOCKER_CERT_PATH="/home/yangzhou/.docker/machine/machines/default"
 # export DOCKER_MACHINE_NAME="default"
 
